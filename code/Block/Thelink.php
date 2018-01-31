9b24baf757f6f07143aeebd3352c96238c362c6a
Dennis Jessen <digidennis3@gmail.com>
2018-01-31 15:19:39 +0100

    init

--- code/Block/Thelink.php
+++ code/Block/Thelink.php
@@ -4,6 +4,6 @@ class Digidennis_QuickpayDolink_Block_Thelink extends Mage_Core_Block_Abstract
 {
     protected function _toHtml()
     {
-        return Mage::getHelper('digidennis_quickpaydolink')->paymentLink($this->getOrder());
+        return Mage::helper('digidennis_quickpaydolink')->paymentLink($this->getOrder());
     }
 }
